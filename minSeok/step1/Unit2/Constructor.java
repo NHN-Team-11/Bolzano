56c632d7859b0b0208ef76b472d89b938f4acb5f
wntkdnj33 <wntkdnj33@gmail.com>
2023-09-12 16:49:42 +0900

    Create Constructor.java

---
+++ minSeok/step1/Unit2/Constructor.java
@@ -0,0 +1,11 @@
+package Unit2;
+
+public class Constructor {
+    public static void main(String[] args) {
+        String s1 = new String("test1");
+        String s2 = new String("test2");
+        s2 = s1;
+        System.out.println(s2);
+    }
+
+}
