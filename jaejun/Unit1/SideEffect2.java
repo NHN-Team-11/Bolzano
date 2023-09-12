8f62b94cb63e7823789223a8412f7a25d1de7882
heen5620 <87135431+heen5620@users.noreply.github.com>
2023-09-12 22:47:06 +0900

    Delete

--- jaejun/Unit1/SideEffect2.java
+++
@@ -1,9 +0,0 @@
-public class SideEffect2 {
-    public static void main(String[] args) {
-        String s = "name surname";
-        StringBuffer sbuf = new StringBuffer(s);
-        sbuf.replace(0,1,s.substring(0, 1).toUpperCase());
-        sbuf.replace(5, 6, s.substring(5, 6).toUpperCase());
-        System.out.println(sbuf.toString());
-    }
-}
