1abee284e77bae66bd9c31c0c06353d598833a7c
Hmm <78787149+wnd06@users.noreply.github.com>
2023-09-16 16:26:43 +0900

    다시 정리

--- Chapter4/jungmin/Exercise_4_9.java
+++ Chapter4/jungmin/Exercise_4_9.java
@@ -3,7 +3,7 @@ package Bolzano.Chapter4.jungmin;
 public class Exercise_4_9 {
     public static void main(String[] args) {
         StringBuilder sb = new StringBuilder();
-        
+
         Product lamp = new Product("Lamp 60 wait");
         sb.append("Before thie loading: " + lamp).append("\n");
         lamp.downLoad(1000);
@@ -12,5 +12,13 @@ public class Exercise_4_9 {
         sb.append(Product.lastCharacter("Good")).append("\n");
 
         System.out.println(sb);
+        int a = 3;
+        int b = 5;
+        int c = a + b;
+        System.out.println(sqr(c));
+    }
+
+    public static int sqr(int x) {
+        return x * x;
     }
 }
