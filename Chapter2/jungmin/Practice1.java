44b5d66152d74f23aee18757b829dcc9728b7dc0
Hmm <78787149+wnd06@users.noreply.github.com>
2023-09-17 21:36:20 +0900

    plus

--- Chapter2/jungmin/Practice1.java
+++ Chapter2/jungmin/Practice1.java
@@ -1,10 +1,15 @@
-
+import javax.swing.JOptionPane;
 
 public class Practice1 {
     public static void main(String[] args) {
         String name = new String("Lee");
         String first = name.substring(0, 1);
         String last = name.substring(name.length() - 1, name.length());
+        String a = "Lee";
+        String b = "Lee";
+        a.concat(last)
+        System.out.println(a == b);
+        System.out.println(name == a);
         System.out.println(first);
         System.out.println(last);
     }
