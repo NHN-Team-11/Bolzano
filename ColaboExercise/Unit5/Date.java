35bc267832b9ae407b39d258677dffd0a8f4ba42
wntkdnj33 <wntkdnj33@gmail.com>
2023-09-21 16:31:32 +0900

    123123

--- ColaboExercise/Unit5/Date.java
+++ ColaboExercise/Unit5/Date.java
@@ -1,119 +1,58 @@
 package ColaboExercise.Unit5;
 
 public class Date {
-
+    private int year;
     private int day;
     private int month;
-    private int year;
 
     public Date(int day, int month, int year) {
-
         this.day = day;
         this.month = month;
         this.year = year;
     }
 
-    public int getDay() {
-        return this.day;
+    public static boolean equalDate(Date date1, Date date2) {
+        return (date1.getDay() == date2.getDay()) && (date1.getMonth() == date2.getMonth())
+                && (date1.getYear() == date2.getYear());
     }
 
-    public int getMonth() {
-        return this.month;
-    }
-
-    public int getYear() {
-        return this.year;
-    }
-
-    public boolean equalTo(Date d) {
-        return this.day == d.day &&
-        this.month == d.month && 
-        this.year == d.year;
-    }
-
-    public boolean procedes(Date d) {
-        if(this.year < d.year) {
+    public static boolean isFowardThenOtherDay(Date date1, Date date2) {
+        if (date1.getYear() < date2.getYear()) {
             return true;
-        }
-        else if(this.year == d.year) {
-            if(this.month < d.month) {
+        } else if (date1.getYear() > date2.getYear()) {
+            return false;
+        } else {
+            if (date1.getMonth() < date2.getMonth()) {
                 return true;
-            }
-            else if(this.month == d.month) {
-                if(this.day < d.day) {
+            } else if (date1.getMonth() > date2.getMonth()) {
+                return false;
+            } else {
+                if (date1.getDay() < date2.getDay()) {
                     return true;
                 }
-                else {
-                    return false;
-                }
-            }
-            else {
                 return false;
             }
         }
-        else return false;
     }
 
-    public boolean leapYear() {
-        if((this.year % 4 == 0 && this.year % 100 != 0) || this.year % 400 == 0) {
+    public static boolean isLeapYear(Date day) {
+        if ((day.getYear() % 4 == 0 && !(day.getYear() % 100 == 0)) || (day.getYear() % 400 == 0)) {
             return true;
-        }
-        else {
+        } else {
             return false;
         }
     }
 
-    public void nextDate() {
-        
-        switch(this.month) {
-            case 1: case 3: case 5: case 7: case 8: case 10: case 12: {
-                this.day += 1;
-                if(this.day > 31) {
-                    this.month += 1;
-                    this.day = 1;
-                }
-                break;
-            }
-            case 2 : {
-                this.day += 1;
-                if(this.day > 28) {
-                    this.month += 1;
-                    this.day = 1;
-                }
-                break;
-            }
-            case 4: case 6: case 9: case 11: {
-                this.day += 1;
-                if(this.day > 30) {
-                    this.month += 1;
-                    this.day = 1;
-                }
-                break;
-            }
-        }
-
-        if(this.month > 12) {
-            this.month -= 12;
-            this.year += 1;
-        }
-        
+    public int getYear() {
+        return year;
     }
 
-    @Override
-    public String toString() {
-        return "Date [" + year + "년 " + month + "월 " + day + "일" + "]";
-    }
-    
-    public static void main(String[] args) {
-        Date date = new Date(31, 12, 2020);
-        Date date2 = new Date(31, 12, 2020);
-        System.out.println(date);
-        System.out.println(date.equalTo(date2));
-        date2.nextDate();
-        System.out.println(date2);
-        System.out.println(date.procedes(date2));
-        System.out.println(date.leapYear());
+    public int getDay() {
+        return day;
     }
 
-    
+    public int getMonth() {
+        return month;
+    }
+
 }
