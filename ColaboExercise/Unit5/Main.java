242fba6185b589816f27e7d2e99a90e189cd2822
JaeJunhahaha <129287148+JaeJunhahaha@users.noreply.github.com>
2023-09-21 14:47:03 +0900

    ㅏㅣ

--- ColaboExercise/Unit5/Main.java
+++ ColaboExercise/Unit5/Main.java
@@ -3,5 +3,6 @@ package ColaboExercise.Unit5;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world");
+        System.out.println("채현명 바보");
     }
 }
