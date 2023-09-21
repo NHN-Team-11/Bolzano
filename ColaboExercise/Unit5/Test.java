5189e79e31c045de0884205bd2b8475575ecaa30
JaeJunhahaha <129287148+JaeJunhahaha@users.noreply.github.com>
2023-09-21 16:07:30 +0900

    ㅎ

--- ColaboExercise/Unit5/Test.java
+++ ColaboExercise/Unit5/Test.java
@@ -2,7 +2,8 @@ package ColaboExercise.Unit5;
 
 public class Test {
     public static void main(String[] args) {
-        System.out.println("채현명 이상"); //이중민 작성
-        System.out.println("이중민 ㅁㄴㅇ"); //채현명 작성
+        System.out.println("채현명 이상"); // 이중민 작성
+        System.out.println("이중민 ㅁㄴㅇ"); // 채현명 작성
+        System.out.println("채현명 진짜 이상해");// 하재준 작성
     }
 }
