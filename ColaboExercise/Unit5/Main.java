786a1cb7b2cb0e696ed2b539256258b60575ba98
wntkdnj33 <wntkdnj33@gmail.com>
2023-09-21 14:49:35 +0900

    good

--- ColaboExercise/Unit5/Main.java
+++ ColaboExercise/Unit5/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world");
         System.out.println("채현명 바보");
+        System.out.println("하재준 노답");
     }
 }
