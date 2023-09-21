47af39d76538ed8e209d339ea2e106cc488574b7
Hmm <78787149+wnd06@users.noreply.github.com>
2023-09-21 15:29:45 +0900

    Update Main.java

--- ColaboExercise/Unit5/Main.java
+++ ColaboExercise/Unit5/Main.java
@@ -5,5 +5,6 @@ public class Main {
         System.out.println("Hello world");
         System.out.println("채현명 바보");
         System.out.println("하재준 노답");
+        System.out.println("채현명 개노답");
     }
 }
