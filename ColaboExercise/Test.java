57ddaddc33f81d4c54e60630204bf357a14d3082
Hmm <78787149+wnd06@users.noreply.github.com>
2023-09-21 16:38:48 +0900

    테스트

--- ColaboExercise/Test.java
+++ ColaboExercise/Test.java
@@ -2,6 +2,7 @@ package ColaboExercise;
 
 public class Test {
     public static void main(String[] args) {
-        System.out.println("테스트 확인");
+        System.out.println("이중민 테스트");
+        System.out.println("채현명 오");
     }
 }
