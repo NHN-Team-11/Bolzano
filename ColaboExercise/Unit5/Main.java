02c5b9966e38d77ae59417d1bc208550ea40a2c0
Hmm <78787149+wnd06@users.noreply.github.com>
2023-09-21 14:40:46 +0900

    ggg

--- ColaboExercise/Unit5/Main.java
+++ ColaboExercise/Unit5/Main.java
@@ -2,6 +2,6 @@ package ColaboExercise.Unit5;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        System.out.println("ggg");
     }
 }
