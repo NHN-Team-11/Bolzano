0d42347a76e01bdce4672bd865202a0d392ca4b2
wntkdnj33 <wntkdnj33@gmail.com>
2023-09-21 16:16:05 +0900

    12341234

--- ColaboExercise/Unit5/Date.java
+++ ColaboExercise/Unit5/Date.java
@@ -46,4 +46,16 @@ public class Date {
         return nextDate;
     }
 
+    public int getDay() {
+        return day;
+    }
+
+    public int getMonth() {
+        return month;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
 }
