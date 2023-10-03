6cc1abcb0a2b1124d780580a2cc4cef74744ecbb
wntkdnj33 <wntkdnj33@gmail.com>
2023-10-04 08:51:23 +0900

    commit

---
+++ Chapter11/minseok/ListNode.java
@@ -0,0 +1,15 @@
+package Chapter11.minseok;
+
+public class ListNode {
+    public String info;
+    public ListNode next;
+
+    public ListNode() {
+    }
+
+    public ListNode(String info) {
+        this.info = info;
+        this.next = null;
+    }
+
+}
